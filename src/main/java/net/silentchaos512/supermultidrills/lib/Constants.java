1ec54238f261623460905e2ee95dd1302b0af715
SilentChaos512 <sworkman256@gmail.com>
2020-04-20 21:08:59 -0500

    Remove unused EvalEx constant for now

--- src/main/java/net/silentchaos512/supermultidrills/lib/Constants.java
+++ src/main/java/net/silentchaos512/supermultidrills/lib/Constants.java
@@ -1,6 +1,5 @@
 package net.silentchaos512.supermultidrills.lib;
 
-import com.udojava.evalex.Expression;
 import net.minecraft.item.Item;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
@@ -15,5 +14,5 @@ public class Constants {
     public static final Tag<Item> DRILL_BATTERIES_TAG = new ItemTags.Wrapper(SuperMultiDrills.getId("drill_batteries"));
 
     // FIXME: Add configs
-    public static final Expression DRILL_ENERGY_EXPR = new Expression("25 + e^(7 - 0.003 * durability) * (1 + 0.06 * efficiency) * hardness");
+//    public static final Expression DRILL_ENERGY_EXPR = new Expression("25 + e^(7 - 0.003 * durability) * (1 + 0.06 * efficiency) * hardness");
 }
